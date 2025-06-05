b0881c4546b8d623b0c84e75f9e962bb8e7a760c
Juan Diego Baron Faura <JDBF1011@Gmail.com>
2025-06-05 10:14:35 +0000

    First commit

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,13 @@
+const Card = () => {
+    return (
+        <div class="card" style={{width: "18rem"}}>
+            <img src="https://png.pngtree.com/background/20250124/original/pngtree-beautiful-natural-scenery-picture-image_15750499.jpg" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    )
+}
+export default Card
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+const Jumbotron = () =>{
+    return(
+        <div className="bg-body-secondary container p-4">
+            <h1 className="p-3"> A War welcome</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis omnis enim dolorem, rem doloribus quo, reiciendis nostrum tenetur odio praesentium quisquam, voluptatem esse asperiores maiores officiis cumque qui. Architecto, eveniet.</p>
+            <button className="btn btn-primary m-3">Call to action!</button>
+        </div>
+    )
+}
+
+export default Jumbotron
